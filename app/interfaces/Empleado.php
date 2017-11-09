4a651fe4b3a9bec4ece9f4f142be7a09e3953a32
carlos.martes <carlos.martes@handycommerce.com.co>
2017-11-09 15:04:59 -0500

    Interface Empleado creada y modelo persona creado

--- app/interfaces/Empleado.php
+++ app/interfaces/Empleado.php
@@ -5,3 +5,23 @@
  * Date: 9/11/17
  * Time: 02:38 PM
  */
+namespace App\Interfaces;
+
+interface EmpleadoInterface
+{
+    public function getById($id);
+    public function getAll();
+    public function create(array $data);
+    public function update(array $data);
+    public function delete($id);
+    public function getSalary();
+    public function setSalary($salary);
+    public function getStatus();
+    public function setStatus($status);
+    public function getCargo();
+    public function setCargo($cargo);
+    public function getTipoCargo();
+    public function setTipoCargo($tipoCargo);
+    public function getJornada();
+    public function setJornada($jornada);
+}
